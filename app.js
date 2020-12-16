d572688213c2071a8e962d651a799e68551f7714
Łukasz <lewak.lukasz@gmail.com>
2020-12-16 16:08:31 +0100

    clock add

---
+++ app.js
@@ -0,0 +1,35 @@
+window.addEventListener('scroll', function () {
+    var header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0);
+});
+
+function toggleMenu() {
+    var menuToggle = document.querySelector('.toggle');
+    var menu = document.querySelector('.menu')
+    menuToggle.classList.toggle('active');
+    menu.classList.toggle('active')
+};
+
+const body = document.querySelector('body');
+const dot = document.querySelector('.dot');
+document.addEventListener('mousemove', (e) => {
+    dot.style.left = e.clientX + 'px';
+    dot.style.top = e.clientY + 'px';
+
+});
+
+function clock() {
+    var hours = document.getElementById('hour');
+    var minutes = document.getElementById('minutes');
+    var seconds = document.getElementById('seconds');
+
+    var h = new Date().getHours();
+    var m = new Date().getMinutes();
+    var s = new Date().getSeconds();
+
+    hours.innerHTML = h;
+    minutes.innerHTML = m;
+    seconds.innerHTML = s;
+}
+var interval = setInterval(clock, 1000);
+
